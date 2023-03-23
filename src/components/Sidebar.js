6b28d08bc2fba642a94f0b31b52e372a4e5d31ac
iamvijaysubramanian <vijaysubbu69@gmail.com>
2023-03-23 22:17:17 +0530

    deployment

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -42,7 +42,7 @@ function Sidebar() {
     });
 
     function getRooms() {
-        fetch("http://localhost:5001/rooms")
+        fetch("https://mern-chat-yl3i.onrender.com/rooms")
             .then((res) => res.json())
             .then((data) => setRooms(data));
     }
